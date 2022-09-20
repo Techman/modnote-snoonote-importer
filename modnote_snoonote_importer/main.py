3fab77b2f11b07293ec165940a3dd649fc12e2d2
Michael Hazell <michaelhazell@hotmail.com>
2022-09-20 01:17:25 -0400

    Argument parsing

--- modnote_snoonote_importer/main.py
+++ modnote_snoonote_importer/main.py
@@ -1,6 +1,23 @@
-"""Import Snoo Notes into Mod Notes"""
+"""Import SnooNotes into Mod Notes"""
+
+from argparse import ArgumentParser
 
 
 def main() -> None:
     """Main function"""
-    print("Hello world")
+
+    # Parse arguments
+    parser = ArgumentParser(description="Import SnooNotes into Reddit Mod Notes")
+    parser.add_argument(
+        "--app_id",
+        type=str,
+        required=True,
+        help="Reddit app_id for the application created for use in this script",
+    )
+    parser.add_argument(
+        "--app_secret",
+        type=str,
+        required=True,
+        help="Reddit secret for the application created for use in this script",
+    )
+    args = parser.parse_args()
