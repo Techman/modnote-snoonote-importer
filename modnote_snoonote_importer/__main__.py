dbf8d4ebdc81f3b2e0af1002b7c2956952491b6e
Michael Hazell <michaelhazell@hotmail.com>
2022-09-20 00:59:03 -0400

    Hello world

---
+++ modnote_snoonote_importer/__main__.py
@@ -0,0 +1,6 @@
+"""__main__.py"""
+
+from modnote_snoonote_importer.main import main
+
+if __name__ == "__main__":
+    main()
---
+++ modnote_snoonote_importer/main.py
@@ -0,0 +1,6 @@
+"""Import Snoo Notes into Mod Notes"""
+
+
+def main() -> None:
+    """Main function"""
+    print("Hello world")
